e0b52a5679136d06115fe58184e252d6f7d83529
Rama Nadar <ramaraghunathan18@gmail.com>
2023-11-07 22:52:37 +0530

    lasssstttt

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -12,59 +12,57 @@ const ItemList = ({ items }) => {
   };
 
   return (
-    <div>
-      <div className="flex p-4">
-        <div className="flex flex-col space-y-2 justify-between w-9/12">
-          {items.map((item) => (
-            <div
-              key={item.card.info.id}
-              className="text-left p-2 my-2 border-b flex flex-row justify-between "
-            >
-              {/* food info */}
-              <div className="flex flex-col w-9/12 p-2">
-                <span className="font-semibold"> {item.card.info.name}</span>
+    <div className="flex p-4 ">
+      <div className="flex flex-col space-y-2 justify-between w-9/12">
+        {items.map((item) => (
+          <div
+            key={item.card.info.id}
+            className="text-left p-2 my-2 border-b flex flex-row justify-between "
+          >
+            {/* food info */}
+            <div className="flex flex-col w-9/12 p-2">
+              <span className="font-semibold"> {item.card.info.name}</span>
 
-                <span>
-                  ₹
-                  {item.card.info.price
-                    ? item.card.info.price / 100
-                    : item.card.info.defaultPrice / 100}
-                </span>
-                <p className="text-slate-500 text-xs line-clamp-2">
-                  {item.card.info.description}
-                </p>
-              </div>
+              <span>
+                ₹
+                {item.card.info.price
+                  ? item.card.info.price / 100
+                  : item.card.info.defaultPrice / 100}
+              </span>
+              <p className="text-slate-500 text-xs line-clamp-2">
+                {item.card.info.description}
+              </p>
+            </div>
 
-              <div className="w-3/12 p-2">
-                <div className="w-28  max-h-24 shrink-0 relative flex justify-center p-2">
-                  {item.card.info.imageId ? (
-                    <div className="w-22 max-h-24 px-0 shrink-0 relative flex justify-center bg-black">
-                      <img
-                        src={CDN_URL + item.card.info.imageId}
-                        loading="lazy"
-                        className="w-28 h-24 object-cover object-center rounded-lg"
-                      />
+            <div className="w-3/12 p-2">
+              <div className="w-28  max-h-24 shrink-0 relative flex justify-center p-2">
+                {item.card.info.imageId ? (
+                  <div className="w-22 max-h-24 px-0 shrink-0 relative flex justify-center bg-black">
+                    <img
+                      src={CDN_URL + item.card.info.imageId}
+                      loading="lazy"
+                      className="w-28 h-24 object-cover object-center rounded-lg"
+                    />
 
-                      <button
-                        className="absolute -bottom-2 w-24 text-center bg-white text-green-600 px-2  py-2 text-xs font-bold border border-slate-300 rounded-lg"
-                        onClick={() => handleAddItem(item)}
-                      >
-                        Add+
-                      </button>
-                    </div>
-                  ) : (
                     <button
                       className="absolute -bottom-2 w-24 text-center bg-white text-green-600 px-2  py-2 text-xs font-bold border border-slate-300 rounded-lg"
                       onClick={() => handleAddItem(item)}
                     >
                       Add+
                     </button>
-                  )}
-                </div>
+                  </div>
+                ) : (
+                  <button
+                    className="absolute -bottom-2 w-24 text-center bg-white text-green-600 px-2  py-2 text-xs font-bold border border-slate-300 rounded-lg"
+                    onClick={() => handleAddItem(item)}
+                  >
+                    Add+
+                  </button>
+                )}
               </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </div>
   );
