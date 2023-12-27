d14bf5d03c41660fd2f6a0cca35c6ae62b8f478c
Rama Raghu <109215335+RamaRaghu1@users.noreply.github.com>
2023-12-27 05:33:22 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,11 @@
 export const CDN_URL =
-  "https://cors-anywhere.herokuapp.com/?https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+  "https://corsproxy.io/?https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const LOGO_URL =
   "https://www.designmantic.com/images/industry/beverage/dm-food-logo-09.png";
 
 export const MENU_URL =
-  "https://cors-anywhere.herokuapp.com/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=13.0826802&lng=80.2707184&restaurantId=";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=13.0826802&lng=80.2707184&restaurantId=";
 export const RESTAURANT_LIST_MOBILE =
   "https://www.swiggy.com/mapi/homepage/getCards?lat=23.369726076519406&lng=85.33414583862306";
 export const RESTAURANT_LIST_DESKTOP =
